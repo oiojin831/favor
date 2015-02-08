61e8dc1ca6d69a297636a365f2edb3a4f17c2402
eungJin <oiojin831@gmail.com>
2015-02-08 23:58:06 +0900

    Finish sign in out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "alert-sucess"
+    when "error"
+      "alert-error"
+    when "alert"
+      "alert-warning"
+    when "notice"
+      "alert-info"
+    else
+      flash_type.to_s
+    end
+  end
 end
