ce6805c0d5f26852b97f6c9b7df4b5a4131ba6e3
eungJin <oiojin831@gmail.com>
2015-03-19 13:06:03 +0900

    Add help?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def help
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'welcome#index'
+  get 'help' => 'welcome#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
